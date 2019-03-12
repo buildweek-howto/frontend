adf794c1fd1c6b13459eb813521811a9b241b916
macjabeth <macbethjonathan@gmail.com>
2019-03-12 18:53:59 +0000

    handle application status on the state

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,6 +3,7 @@ import { combineReducers } from 'redux';
 import categories from './categories';
 import currentUser from './currentUser';
 import posts from './posts';
+import status from './status';
 import users from './users';
 
 export default combineReducers({
