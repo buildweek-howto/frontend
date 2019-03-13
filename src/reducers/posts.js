20298df987eb94d0e061bc5a59016dee1680a90a
macjabeth <macbethjonathan@gmail.com>
2019-03-13 18:13:02 +0000

    fix category reducer & add selector functions to the posts reducer

--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -6,16 +6,33 @@ export default (state = [], action) => {
     case ADD_POST_SUCCESS:
     case EDIT_POST_SUCCESS:
     case DELETE_POST_SUCCESS:
+
       return action.payload;
     default:
       return state;
   }
 };
 
-export const getFilterPostsByUser = (state, userId) => {
+export const getPostsByUser = (state, userId) => {
   if (!userId) {
     return state;
   } else {
     return state.filter(post => post.category_id === userId);
   }
 };
+
+export const getPostCategories = ({posts_categories}, id) => posts_categories.reduce(
+  (acc,cur) => cur.post_id === id ? [...acc, cur.category_id] : acc,
+[]);
+
+export const getCategoryPosts = ({posts_categories}, id) => posts_categories.reduce(
+  (acc, cur) => cur.category_id === id ? [ ...acc, cur.post_id ] : acc,
+[]);
+
+export const getPostsByCategory = (state, id) => {
+  const validPosts = getCategoryPosts(state, id);
+  return {
+    ...state,
+    allPosts: state.allPosts.filter(post => validPosts.includes(post.id))
+  }
+}
