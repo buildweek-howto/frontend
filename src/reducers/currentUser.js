7efc48c01b1c162e5803f6c3e3dc46343f303629
macjabeth <macbethjonathan@gmail.com>
2019-03-14 19:20:16 +0000

    save current user to local storage

--- src/reducers/currentUser.js
+++ src/reducers/currentUser.js
@@ -1,9 +1,16 @@
 import { LOGIN_SUCCESS, SIGNUP_SUCCESS } from "../actions/types";
 
-export default (state = null, action) => {
+const loadUser = () => {
+  const serializedState = localStorage.getItem('currentUser');
+  return Boolean(serializedState) ? JSON.parse(serializedState) : null;
+};
+
+export default (state = loadUser(), action) => {
   switch (action.type) {
     case SIGNUP_SUCCESS:
     case LOGIN_SUCCESS:
+      const serializedState = JSON.stringify(action.payload.currentUser);
+      localStorage.setItem('currentUser', serializedState);
       return action.payload.currentUser;
     default:
       return state;
